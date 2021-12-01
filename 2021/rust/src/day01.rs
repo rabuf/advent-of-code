a9df566f679de3c9200d7af0d074c9e3aacb2713
Jared Summers <jtsummers@gmail.com>
2021-12-01 13:09:09 -0700

    2021.01[Rust] Moved day01 into its own module.

---
+++ 2021/rust/src/day01.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn day01_01() -> i64 {
+    let filename = "../input/01.txt";
+    let file = match File::open(filename) {
+        Ok(f) => f,
+        Err(why) => panic!("Error opening {}: {}", filename, why),
+    };
+    let reader = BufReader::new(file);
+    reader
+        .lines()
+        .map(|l| l.unwrap().parse::<i64>().unwrap())
+        .fold((0 as i64, i64::max_value()), |(sum, prev), curr| {
+            (sum + if prev < curr { 1 } else { 0 }, curr)
+        })
+        .0
+}
+pub fn day01_02() -> i64 {
+    let filename = "../input/01.txt";
+    let file = match File::open(filename) {
+        Ok(f) => f,
+        Err(why) => panic!("Error opening {}: {}", filename, why),
+    };
+    let reader = BufReader::new(file);
+    reader
+        .lines()
+        .map(|l| l.unwrap().parse::<i64>().unwrap())
+        .fold(
+            (
+                0 as i64,
+                (i64::max_value(), i64::max_value(), i64::max_value()),
+            ),
+            |(sum, (prev, b, c)), curr| (sum + if prev < curr { 1 } else { 0 }, (b, c, curr)),
+        )
+        .0
+}
--- 2021/rust/src/main.rs
+++ 2021/rust/src/main.rs
@@ -1,42 +1,7 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+mod day01;
 
-fn day01_01() -> i64 {
-    let filename = "../input/01.txt";
-    let file = match File::open(filename) {
-        Ok(f) => f,
-        Err(_) => panic!("{} not found.", filename),
-    };
-    let reader = BufReader::new(file);
-    reader
-        .lines()
-        .map(|l| l.unwrap().parse::<i64>().unwrap())
-        .fold((0 as i64, i64::max_value()), |(sum, prev), curr| {
-            (sum + if prev < curr { 1 } else { 0 }, curr)
-        })
-        .0
-}
-fn day01_02() -> i64 {
-    let filename = "../input/01.txt";
-    let file = match File::open(filename) {
-        Ok(f) => f,
-        Err(_) => panic!("{} not found.", filename),
-    };
-    let reader = BufReader::new(file);
-    reader
-        .lines()
-        .map(|l| l.unwrap().parse::<i64>().unwrap())
-        .fold(
-            (
-                0 as i64,
-                (i64::max_value(), i64::max_value(), i64::max_value()),
-            ),
-            |(sum, (prev, b, c)), curr| (sum + if prev < curr { 1 } else { 0 }, (b, c, curr)),
-        )
-        .0
-}
 fn main() {
     println!("Hello AoC 2021!");
-    println!("Day 01 Part 01: {0}", day01_01());
-    println!("Day 01 Part 02: {0}", day01_02());
+    println!("Day 01 Part 01: {}", day01::day01_01());
+    println!("Day 01 Part 02: {}", day01::day01_02());
 }
