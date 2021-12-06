c7fc46e212a486554c3597b8b8c803fb29288b8d
Jared Summers <jtsummers@gmail.com>
2021-12-05 20:21:18 -0700

    2021.05[Rust] Optimizations
    Swapped HashMap for Vec<Vec>. Sped up the program by 3-4x. Extra
    computation on the counting step, but less time allocating on the
    construction end.

--- 2021/rust/src/day05.rs
+++ 2021/rust/src/day05.rs
@@ -1,75 +1,77 @@
 use regex::Regex;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-type Point = (i64, i64);
+type Point = (isize, isize);
 type Segment = (Point, Point);
 
-fn get_input(filename: &str) -> Vec<Segment> {
+fn get_input(filename: &str) -> (Vec<Segment>, (usize, usize)) {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
     let lines = reader.lines();
     let re = Regex::new(r"(\d+),(\d+) -> (\d+),(\d+)").unwrap();
     let mut segments: Vec<Segment> = vec![];
+    let mut max_x = 0;
+    let mut max_y = 0;
     for line in lines.map(|l| l.unwrap()) {
         match re.captures(&line) {
             None => (),
             Some(cap) => {
-                segments.push((
-                    (
-                        i64::from_str_radix(&cap[1], 10).unwrap(),
-                        i64::from_str_radix(&cap[2], 10).unwrap(),
-                    ),
-                    (
-                        i64::from_str_radix(&cap[3], 10).unwrap(),
-                        i64::from_str_radix(&cap[4], 10).unwrap(),
-                    ),
-                ));
+                let x0 = isize::from_str_radix(&cap[1], 10).unwrap();
+                let y0 = isize::from_str_radix(&cap[2], 10).unwrap();
+                let x1 = isize::from_str_radix(&cap[3], 10).unwrap();
+                let y1 = isize::from_str_radix(&cap[4], 10).unwrap();
+                segments.push(((x0,y0),(x1,y1)));
+                max_x = max_x.max(x0.max(x1));
+                max_y = max_y.max(y0.max(y1));
             }
         }
     }
-    segments
+    (segments, ((max_x + 1) as usize, (max_y + 1) as usize))
 }
 
-fn make_grid(segments: &[Segment]) -> HashMap<Point, u64> {
-    let mut grid: HashMap<Point, u64> = HashMap::new();
+fn make_grid(segments: &[Segment], x: usize, y: usize) -> Vec<Vec<usize>> {
+    let mut grid= vec![vec![0;y];x];
+
     for ((x0, y0), (x1, y1)) in segments {
-        let dx = i64::signum(x1 - x0);
-        let dy = i64::signum(y1 - y0);
-        let distance = i64::max(i64::abs(x1 - x0), i64::abs(y1 - y0));
+        let dx = (x1 - x0).signum();
+        let dy = (y1 - y0).signum();
+        let distance = (x1 - x0).abs().max((y1 - y0).abs());
 
         for i in 0..=distance {
-            let p = (x0 + dx * i, y0 + dy * i);
-            match grid.get_mut(&p) {
-                None => {
-                    grid.insert(p, 1);
-                } //.insert(p, 1).unwrap(),
-                Some(x) => *x = *x + 1,
-            }
+            let (x,y) = ((x0 + dx * i) as usize, (y0 + dy * i) as usize);
+            grid[x][y] = grid[x][y] + 1;
         }
     }
     grid
 }
 
-fn count_overlapping(grid: HashMap<Point, u64>) -> usize {
-    grid.iter().filter(|(_k, v)| **v > 1).count()
+fn count_overlapping(grid: &Vec<Vec<usize>>) -> isize {
+    let mut count = 0;
+    for row in grid.iter() {
+        for cell in row.iter() {
+            if *cell > 1 {
+                count = count + 1;
+            }
+        }
+    }
+    count
 }
 
-pub fn part1() -> usize {
-    let segments = get_input("../input/05.txt");
+pub fn part1() -> isize {
+    let (segments, (x, y)) = get_input("../input/05.txt");
     let filtered = segments
         .into_iter()
         .filter(|((x0, y0), (x1, y1))| x0 == x1 || y0 == y1)
         .collect::<Vec<Segment>>();
-    let grid = make_grid(&filtered);
-    count_overlapping(grid)
+    let grid = make_grid(&filtered, x, y);
+    count_overlapping(&grid)
 }
 
-pub fn part2() -> usize {
-    let segments = get_input("../input/05.txt");
-    let grid = make_grid(&segments);
-    count_overlapping(grid)
+pub fn part2() -> isize {
+    let (segments, (x, y)) = get_input("../input/05.txt");
+    let grid = make_grid(&segments, x, y);
+    count_overlapping(&grid)
 }
 
 #[cfg(test)]
