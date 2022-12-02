054c80f927ae33205240b5776ac8869e099dc81b
Jared Summers <jtsummers@gmail.com>
2022-12-01 20:48:57 -0700

    2022.01[Python] - Both parts & tests

---
+++ 2022/python/day01.py
@@ -0,0 +1,25 @@
+import sys
+from pathlib import Path
+
+
+def solve(elves):
+    return elves[-1], sum(elves[-3:])
+
+
+def parse(elves):
+    result = []
+    for elf in elves.split("\n\n"):
+        calories = sum(int(calories) for calories in elf.split("\n"))
+        result.append(calories)
+    return sorted(result)
+
+
+def main():
+    input_path = Path(sys.argv[1])
+    with open(input_path / "01.txt", "r") as f:
+        elves = parse(f.read().strip())
+        print(solve(elves))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2022/python/test_day01.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from hypothesis import given, strategies as st
+import day01
+
+
+class Test(TestCase):
+    sample = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
+    @given(st.lists(st.integers(), min_size=1))
+    def test_solve(self, elves):
+        elves = sorted(elves)
+        a, b = day01.solve(elves)
+        self.assertEqual(a, max(elves))
+        self.assertEqual(b, sum(elves[-3:]))
+
+    def test_sample(self):
+        elves = day01.parse(self.sample)
+        self.assertListEqual(elves, [4000, 6000, 10000, 11000, 24000])
+        a, b = day01.solve(elves)
+        self.assertEqual(a, 24000)
+        self.assertEqual(b, 45000)
