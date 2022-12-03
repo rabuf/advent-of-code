c6bb7e37d66449dbe52be591ed8459c19dd61713
Jared Summers <jtsummers@gmail.com>
2022-12-03 13:29:14 -0700

    2022.02[Python] - Cleaned up Hypothesis tests
    Added a custom generator for RPS plays so the Hypothesis tests could
    be simplified. Now they read more simply as:
    
    ```python
    @given(rps_play)
    def test_foo(self, play):
        ...
    ```

--- 2022/python/test_day02.py
+++ 2022/python/test_day02.py
@@ -4,6 +4,9 @@ from hypothesis import given, strategies as st
 
 
 class Test(TestCase):
+    # Custom strategy for generating RPS play. Used by hypothesis tests.
+    rps_play = st.integers(min_value=1, max_value=3).map(RPS)
+
     def test_values(self):
         self.assertEqual(1, RPS.ROCK)
         self.assertEqual(2, RPS.PAPER)
@@ -19,21 +22,18 @@ class Test(TestCase):
         self.assertEqual((RPS.PAPER, RPS.PAPER), parse_line("B Y"))
         self.assertEqual((RPS.SCISSORS, RPS.SCISSORS), parse_line("C Z"))
 
-    @given(st.integers(min_value=1, max_value=3))
+    @given(rps_play)
     def test_score_equals_play_on_loss(self, opp):
-        opp = RPS(opp)
         loss = a_beats_b[opp]
         self.assertEqual(loss, score(opp, loss))
 
-    @given(st.integers(min_value=1, max_value=3))
+    @given(rps_play)
     def test_score_equals_play_plus_three_on_draw(self, opp):
-        opp = RPS(opp)
         tie = opp
         self.assertEqual(tie + 3, score(opp, tie))
 
-    @given(st.integers(min_value=1, max_value=3))
+    @given(rps_play)
     def test_score_equals_play_plus_six_on_win(self, opp):
-        opp = RPS(opp)
         win = a_beats_b[a_beats_b[opp]]
         self.assertEqual(win + 6, score(opp, win))
 
@@ -51,20 +51,17 @@ class Test(TestCase):
         self.assertEqual(a, 15)
         self.assertEqual(b, 12)
 
-    @given(st.integers(min_value=1, max_value=3))
+    @given(rps_play)
     def test_strategy_rock_loses(self, opp):
-        opp = RPS(opp)
         play = strategy(opp, RPS.ROCK)
         self.assertTrue(beats(opp, play))
 
-    @given(st.integers(min_value=1, max_value=3))
+    @given(rps_play)
     def test_strategy_paper_ties(self, opp):
-        opp = RPS(opp)
         play = strategy(opp, RPS.PAPER)
         self.assertEqual(opp, play)
 
-    @given(st.integers(min_value=1, max_value=3))
+    @given(rps_play)
     def test_strategy_scissors_wins(self, opp):
-        opp = RPS(opp)
         play = strategy(opp, RPS.SCISSORS)
         self.assertTrue(beats(play, opp))
