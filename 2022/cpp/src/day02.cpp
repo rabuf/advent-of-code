c55342d7ee56222eb1a6e36309c875a2d6732d91
Jared Summers <jtsummers@gmail.com>
2022-12-02 19:56:50 -0700

    2022.02[C++] - Fun with accumulators and pairs
    One loop over the plays to get both scores now.

--- 2022/cpp/src/day02.cpp
+++ 2022/cpp/src/day02.cpp
@@ -6,9 +6,9 @@
 
 #include "day02.h"
 
-std::unordered_map<rps,rps> beats = {
-        {rps::rock, rps::scissors},
-        {rps::paper, rps::rock},
+std::unordered_map<rps, rps> beats = {
+        {rps::rock,     rps::scissors},
+        {rps::paper,    rps::rock},
         {rps::scissors, rps::paper},
 };
 
@@ -16,13 +16,18 @@ int score(rps left, rps right) {
     return std::to_underlying(right) + 3 * ((left == right) + 2 * (left == beats[right]));
 }
 
-std::pair<int, int> day02_solve(std::vector<std::pair<rps,std::pair<rps, rps>>> input) {
-    int result_1 = std::accumulate(input.begin(), input.end(), 0, [](auto acc, auto play) { return acc + score(play.first, play.second.first); });
-    int result_2 = std::accumulate(input.begin(), input.end(), 0, [](auto acc, auto play) { return acc + score(play.first, play.second.second); });
-    return {result_1, result_2};
+std::pair<int, int> day02_solve(std::vector<std::pair<rps, std::pair<rps, rps>>> input) {
+    auto result = std::accumulate(input.begin(), input.end(), std::make_pair(0, 0),
+                                  [](auto acc, auto play) {
+                                      auto [them, me] = play;
+                                      auto [first, second] = me;
+                                      return std::make_pair(acc.first + score(them, first),
+                                                            acc.second + score(them, second));
+                                  });
+    return result;
 }
 
-void day02_parse(std::istream &in, std::vector<std::pair<rps,std::pair<rps, rps>>> &result) {
+void day02_parse(std::istream &in, std::vector<std::pair<rps, std::pair<rps, rps>>> &result) {
     for (std::string line; std::getline(in, line);) {
         rps left{(line[0] - 'A') + 1};
         std::pair<rps, rps> right;
@@ -44,9 +49,9 @@ void day02_parse(std::istream &in, std::vector<std::pair<rps,std::pair<rps, rps>
 
 void day02(const std::string &filename) {
     std::ifstream in{filename};
-    std::vector<std::pair<rps,std::pair<rps, rps>>> plays;
+    std::vector<std::pair<rps, std::pair<rps, rps>>> plays;
     day02_parse(in, plays);
-    auto [a,b] = day02_solve(plays);
+    auto [a, b] = day02_solve(plays);
     std::cout << "Day 02:\n";
     std::cout << "    Part 1: " << a << std::endl;
     std::cout << "    Part 2: " << b << std::endl;
