4439089ab611ed43db9606967a04af2c550879f4
Jared Summers <jtsummers@gmail.com>
2022-12-11 21:52:23 -0700

    2022.09[Python] Both parts

---
+++ 2022/python/aoc2022/day09.py
@@ -0,0 +1,55 @@
+import sys
+from pathlib import Path
+
+from aoc_util import print_day
+
+directions = {
+    'U': 1j,
+    'D': -1j,
+    'R': 1,
+    'L': -1,
+}
+
+
+def sign(x):
+    return (x > 0) - (x < 0)
+
+
+def move(snake):
+    for i in range(1, len(snake)):
+        dist = snake[i-1] - snake[i]
+        if abs(dist) >= 2:
+            snake[i] += complex(sign(dist.real), sign(dist.imag))
+    return snake
+
+
+def parse_line(line):
+    direction, distance = line.split()
+    return directions[direction], int(distance)
+
+
+def apply_direction(direction, snake):
+    snake[0] += direction
+    move(snake)
+
+
+def part_1(lines, length=2):
+    snake = [0] * length
+    locations = [{x} for x in snake]
+    for (direction, dist) in lines:
+        for _ in range(dist):
+            apply_direction(direction, snake)
+            [locations[i].add(snake[i]) for i in range(length)]
+    return locations
+
+
+def main():
+    input_dir = Path(sys.argv[1])
+    with open(input_dir / "09.txt") as f:
+        lines = list(map(parse_line, f.read().splitlines()))
+        visited = part_1(lines, length=10)
+        print_day(9, len(visited[1]), len(visited[9]))
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2022/python/tests/test_day09.py
@@ -0,0 +1,91 @@
+import pytest
+from hypothesis.stateful import RuleBasedStateMachine, rule, invariant, initialize
+
+from aoc2022 import day09
+
+small_sample = ("R 4\n"
+                "U 4\n"
+                "L 3\n"
+                "D 1\n"
+                "R 4\n"
+                "D 1\n"
+                "L 5\n"
+                "R 2")
+small_dirs = [(1, 4),
+              (1j, 4),
+              (-1, 3),
+              (-1j, 1),
+              (1, 4),
+              (-1j, 1),
+              (-1, 5),
+              (1, 2),
+              ]
+
+big_sample = ("R 5\n"
+              "U 8\n"
+              "L 8\n"
+              "D 3\n"
+              "R 17\n"
+              "D 10\n"
+              "L 25\n"
+              "U 20\n")
+
+
+@pytest.mark.parametrize('line, expected', zip(small_sample.splitlines(), small_dirs))
+def test_parse_line(line, expected):
+    assert day09.parse_line(line) == expected
+
+
+class SnakeGame(RuleBasedStateMachine):
+    """A state machine for the snake game. Using stateful testing to discover errors in the movement function"""
+
+    snake = None
+    old = None
+
+    @initialize()
+    def set_snake(self):
+        self.snake = [0, 0]
+        self.old = [0, 0]
+
+    @invariant()
+    def always_touching(self):
+        """This test ensures that after movement the tail is within one of the head."""
+        assert int(self.snake[0].real - self.snake[1].real) in [1, 0, -1] and int(self.snake[0].real - self.snake[1].real) in [1, 0, -1]
+
+    @rule()
+    def move_up(self):
+        self.old = self.snake.copy()
+        self.snake[0] += 1j
+        day09.move(self.snake)
+
+    @rule()
+    def move_down(self):
+        self.old = self.snake.copy()
+        self.snake[0] -= 1j
+        day09.move(self.snake)
+
+    @rule()
+    def move_right(self):
+        self.old = self.snake.copy()
+        self.snake[0] += 1
+        day09.move(self.snake)
+
+    @rule()
+    def move_left(self):
+        self.old = self.snake.copy()
+        self.snake[0] -= 1
+        day09.move(self.snake)
+
+
+TestMoveTail = SnakeGame().TestCase
+
+
+def test_small_sample():
+    locs = day09.part_1(small_dirs)
+    print(locs)
+    assert 13 == len(locs)
+
+
+if __name__ == '__main__':
+    print(pytest.__version__)
+    pytest.main()
