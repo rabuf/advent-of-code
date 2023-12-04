2b24f36b38cba33212b24c094702f190461eeac6
Jared Summers <jtsummers@gmail.com>
2023-12-04 13:45:58 -0700

    [Python][2023.04] Code cleanup
    I moved some operations to earlier (converting the
    inputs to sets) and ran a code cleanup on the whole
    thing.

--- python/aoc2023/day04.py
+++ python/aoc2023/day04.py
@@ -1,30 +1,26 @@
 import sys
 from pathlib import Path
-import re
 
 from aoc_util import print_day
 
 
 def score(winning, numbers):
-    matches = set(winning).intersection(numbers)
-    return 2**(len(matches) - 1) if matches else 0
+    matches = len(winning & numbers)
+    return 2 ** (matches - 1) if matches else 0
 
 
 def copies(cards):
     counts = [1] * len(cards)
-    for i in range(len(cards)):
-        winning, numbers = cards[i]
-        matches = len(set(winning).intersection(numbers))
-        for j in range(i+1, i+1+matches):
+    for (i, (winning, numbers)) in enumerate(cards, start=0):
+        matches = len(winning & numbers)
+        for j in range(i + 1, i + 1 + matches):
             counts[j] += counts[i]
     return counts
 
 
-def parse_line(line):
-    winning, numbers = line.split(':')[1].split(' | ')
-    winning = re.findall(r'\d+', winning)
-    numbers = re.findall(r'\d+', numbers)
-    return winning, numbers
+def parse_line(line: str):
+    winning, numbers = line.split(':')[1].split('|')
+    return set(winning.split()), set(numbers.split())
 
 
 def main():
