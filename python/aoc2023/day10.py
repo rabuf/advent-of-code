5c39aec103bd9e4cc642486d0082f45906dadb52
Jared Summers <jtsummers@gmail.com>
2023-12-10 10:47:28 -0700

    2023.10[Python] Continued refactoring
    I now produce left and right sets of points. Depending on
    the turning one or the other becomes the frontier. Next
    step is to combine their creation.

--- python/aoc2023/day10.py
+++ python/aoc2023/day10.py
@@ -50,20 +50,20 @@ def enclosed_area(grid, start):
     start_direction = initial_direction(grid, start)
     pos = start + start_direction
     direction = start_direction
-    loop_only = {}
-    loop_only[start] = 'S'
+    loop_only = {start: 'S'}
     turns = 0
     while grid[pos] != 'S':
         turns += turning[(grid[pos], direction)]
         direction = next_direction[(grid[pos], direction)]
         loop_only[pos] = grid[pos]
         pos = pos + direction
-    frontier = set()
+    left = set()
+    right = set()
     not_loop = set(grid) - set(loop_only)
 
-    def check(pos):
+    def check(pos, side):
         if pos in not_loop:
-            frontier.add(pos)
+            side.add(pos)
             not_loop.remove(pos)
 
     pos = start + start_direction
@@ -72,47 +72,49 @@ def enclosed_area(grid, start):
         if turns < 0:
             match grid[pos], direction:
                 case ('|', -1j):
-                    check(pos + 1)
+                    check(pos + 1, right)
                 case ('|', 1j):
-                    check(pos - 1)
+                    check(pos - 1, right)
                 case ('-', 1):
-                    check(pos + 1j)
+                    check(pos + 1j, right)
                 case ('-', -1):
-                    check(pos - 1j)
+                    check(pos - 1j, right)
                 case ('J', 1):
-                    check(pos + 1j)
-                    check(pos + 1)
+                    check(pos + 1j, right)
+                    check(pos + 1, right)
                 case ('L', 1j):
-                    check(pos - 1)
-                    check(pos + 1j)
+                    check(pos - 1, right)
+                    check(pos + 1j, right)
                 case ('7', -1j):
-                    check(pos + 1)
-                    check(pos - 1j)
+                    check(pos + 1, right)
+                    check(pos - 1j, right)
                 case ('F', -1):
-                    check(pos - 1)
-                    check(pos - 1j)
+                    check(pos - 1, right)
+                    check(pos - 1j, right)
+            frontier = right
         else:
             match grid[pos], direction:
                 case ('|', -1j):
-                    check(pos - 1)
+                    check(pos - 1, left)
                 case ('|', 1j):
-                    check(pos + 1)
+                    check(pos + 1, left)
                 case ('-', 1):
-                    check(pos - 1j)
+                    check(pos - 1j, left)
                 case ('-', -1):
-                    check(pos + 1j)
+                    check(pos + 1j, left)
                 case ('J', 1j):
-                    check(pos + 1j)
-                    check(pos + 1)
+                    check(pos + 1j, left)
+                    check(pos + 1, left)
                 case ('L', -1):
-                    check(pos - 1)
-                    check(pos + 1j)
+                    check(pos - 1, left)
+                    check(pos + 1j, left)
                 case ('7', 1):
-                    check(pos + 1)
-                    check(pos - 1j)
+                    check(pos + 1, left)
+                    check(pos - 1j, left)
                 case ('F', -1j):
-                    check(pos - 1)
-                    check(pos - 1j)
+                    check(pos - 1, left)
+                    check(pos - 1j, left)
+            frontier = left
         direction = next_direction[(grid[pos], direction)]
         pos = pos + direction
     for pos in list(not_loop):
@@ -130,11 +132,11 @@ def enclosed_area(grid, start):
 
 
 def initial_direction(grid, start):
-    if (start - 1j) in grid and grid[start + -1j] in '7F|':
+    if grid.get(start - 1j, '.') in '7F|':
         start_direction = -1j
-    elif (start + 1j) in grid and grid[start + 1j] in 'LJ|':
+    elif grid.get(start + 1j, '.') in 'LJ|':
         start_direction = 1j
-    elif (start + 1) in grid and grid[start + 1] in '-J7':
+    elif grid.get(start + 1, '.') in '-J7':
         start_direction = 1
     else:
         start_direction = -1
@@ -154,7 +156,7 @@ def loop_length(grid, start):
 
 def main():
     input_dir = Path(sys.argv[1])
-    with open(input_dir / "2023" / "10.test.4.txt") as f:
+    with open(input_dir / "2023" / "10.txt") as f:
         grid, start = input_to_grid(f.read().splitlines())
         length = loop_length(grid, start)
         enclosed = enclosed_area(grid, start)
