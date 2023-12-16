13f65b3c84d2b89fd9fbf13f45044e7e9e59cb08
Jared Summers <jtsummers@gmail.com>
2023-12-16 01:41:31 -0700

    2023.16[Python] Performance enhancements.
    Shaved ~0.5s off by using a dict[dir,set[pos]] instead of dict[pos,set[dir]].

---
+++ python/aoc2023/day16.py
@@ -0,0 +1,87 @@
+import datetime
+from operator import or_
+import sys
+from collections import defaultdict
+from functools import reduce
+from pathlib import Path
+
+from aoc_util import print_day
+
+
+def energized(grid, start=(0 + 0j), direction=(1 + 0j)):
+    state = [(direction, start)]
+    e = defaultdict(set)
+    while state:
+        direction, position = state.pop()
+        if position not in grid:
+            continue
+        if position in e[direction]:
+            continue
+        e[direction].add(position)
+        c = grid[position]
+        match c:
+            case '.':
+                state.append((direction, position + direction))
+            case '|':
+                if direction == 1j or direction == -1j:
+                    state.append((direction, position + direction))
+                else:
+                    state.append((-1j, position - 1j))
+                    state.append((1j, position + 1j))
+            case '-':
+                if direction == 1 or direction == -1:
+                    state.append((direction, position + direction))
+                else:
+                    state.append((-1, position - 1))
+                    state.append((1, position + 1))
+            case '/':
+                if direction == 1 or direction == -1:
+                    state.append((direction * -1j, position + direction * -1j))
+                else:
+                    state.append((direction * 1j, position + direction * 1j))
+            case '\\':
+                if direction == 1 or direction == -1:
+                    state.append((direction * 1j, position + direction * 1j))
+                else:
+                    state.append((direction * -1j, position + direction * -1j))
+    return len(reduce(or_, e.values()))
+
+
+def lines_to_grid(lines):
+    grid = {}
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            grid[x + y * 1j] = c
+    return grid
+
+
+def maximize_energized_region(grid, height, width):
+    energy = 0
+    for y in range(height):
+        energy = max(energy, energized(grid, start=(0 + y * 1j), direction=1))
+        energy = max(energy, energized(grid, start=(width - 1 + y * 1j), direction=-1))
+    for x in range(width):
+        energy = max(energy, energized(grid, start=x, direction=1j))
+        energy = max(energy, energized(grid, start=(x + (height - 1) * 1j), direction=-1j))
+    return energy
+
+
+def main():
+    input_dir = Path(sys.argv[1])
+    with open(input_dir / "2023" / "16.txt") as f:
+        lines = f.read().splitlines()
+    height, width = len(lines), len(lines[0])
+    grid = lines_to_grid(lines)
+    start = datetime.datetime.now()
+    energy = energized(grid)
+    end = datetime.datetime.now()
+    print(f'Part 1: {end - start}')
+    start = datetime.datetime.now()
+    max_energy = maximize_energized_region(grid, height, width)
+    end = datetime.datetime.now()
+    print(f'Brute Force: {end - start}')
+    print_day(16, energy, max_energy)
+
+
+if __name__ == '__main__':
+    main()
