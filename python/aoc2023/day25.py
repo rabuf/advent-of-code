a4d5879a214ecf14261375e04c604f6749bdc5d6
Jared Summers <jtsummers@gmail.com>
2023-12-31 13:46:47 -0700

    2023.25[Python] Switched partition algorithm
    Digging through networkx docs I saw `kernighan_lin_bisection`. This does what I want and does it faster than my prior solution, cutting my execution time by about 80% (on an already fast problem, so not critical).

--- python/aoc2023/day25.py
+++ python/aoc2023/day25.py
@@ -1,10 +1,8 @@
 import sys
-from functools import reduce
-from operator import mul
 from pathlib import Path
 
 import networkx
-from networkx.algorithms.community import greedy_modularity_communities
+from networkx.algorithms.community import kernighan_lin_bisection
 
 from aoc_util import print_day
 
@@ -20,8 +18,8 @@ def main():
     with open(input_dir / "2023" / "25.txt") as f:
         lines = map(parse_line, f.read().splitlines())
         graph = networkx.to_networkx_graph(dict(lines))
-        communities = greedy_modularity_communities(graph, cutoff=2, best_n=2)
-        p1 = reduce(mul, map(len, communities))
+        p1, p2 = kernighan_lin_bisection(graph)
+        p1 = len(p1) * len(p2)
         print_day(25, p1, "FREEBIE!!")
 
 
