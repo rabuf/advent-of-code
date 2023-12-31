18ab70dca3888ed87377ad278998fe15bc9269a8
Jared Summers <jtsummers@gmail.com>
2023-12-30 23:57:23 -0700

    2023.24[Python] Cleaned up

--- python/aoc2023/day24.py
+++ python/aoc2023/day24.py
@@ -1,5 +1,6 @@
 import re
 import sys
+from itertools import combinations
 from pathlib import Path
 
 import z3
@@ -12,24 +13,18 @@ def parse_line(line):
     return [int(n) for n in re.findall(r'-?\d+', line)]
 
 
-def shift_stone(stone, lower=7, upper=27):
+def extend_line(stone, lower=7, upper=27):
     [x, y, z, dx, dy, dz] = stone
-    m = dy / dx
-    b = y - x * m
-    left = Point((lower, m * lower + b))
-    right = Point((upper, m * upper + b))
-    return LineString([left, right])
+    origin = Point((x, y))
+    distance = 0
+    if dx > 0:
+        distance = (upper - x) / dx
+    if dx < 0:
+        distance = (lower - x) / dx
+    return LineString([origin, Point((x + distance * dx, y + distance * dy))])
 
 
-def sign(n):
-    if n < 0:
-        return -1
-    if n > 0:
-        return 1
-    return 0
-
-
-def solve_p2_z3(hailstones):
+def solve_p2(hailstones):
     [x, y, z, dx, dy, dz] = [z3.Int(name) for name in ['x', 'y', 'z', 'dx', 'dy', 'dz']]
     times = z3.IntVector("times", len(hailstones))
     s = z3.Solver()
@@ -42,26 +37,21 @@ def solve_p2_z3(hailstones):
     return m[x].as_long() + m[y].as_long() + m[z].as_long()
 
 
+def solve_p1(hailstones, lower, upper):
+    region = box(lower, lower, upper, upper)
+    extended = [extend_line(stone, lower=lower, upper=upper) & region for stone in hailstones]
+    return sum(bool(s1 & s2) for s1, s2 in combinations(extended, 2))
+
+
 def main():
     input_dir = Path(sys.argv[1])
     with open(input_dir / "2023" / "24.txt") as f:
         hailstones = list(map(parse_line, f.read().splitlines()))
+        # lower, upper = 7, 27
         lower, upper = 200000000000000, 400000000000000
-        region = box(lower, lower, upper, upper)
-        regionalized = [shift_stone(stone, lower=lower, upper=upper) for stone in hailstones]
-        count = 0
-        for i, s1 in enumerate(regionalized[:-1]):
-            for j, s2 in enumerate(regionalized[i + 1:], start=i + 1):
-                intersection = s1.intersection(s2)
-                intersection = intersection.intersection(region)
-                if intersection:
-                    if (sign(intersection.x - hailstones[i][0]) == sign(hailstones[i][3])
-                            and sign(intersection.x - hailstones[j][0]) == sign(hailstones[j][3])
-                            and sign(intersection.y - hailstones[i][1]) == sign(hailstones[i][4])
-                            and sign(intersection.y - hailstones[j][1]) == sign(hailstones[j][4])):
-                        count += 1
-        p2 = solve_p2_z3(hailstones)
-        print_day(24, count, p2)
+        p1 = solve_p1(hailstones, lower, upper)
+        p2 = solve_p2(hailstones)
+        print_day(24, p1, p2)
 
 
 if __name__ == '__main__':
