5b5ab660edde1e34e760025c6e0501c5c7e749b7
Jared Summers <jtsummers@gmail.com>
2024-01-01 01:19:09 -0700

    2023.21[Python] A bit of dynamic programming
    Sped the program up about 15-30x by using a DP approach in `infinite_gardens`.

--- python/tests/test2023/test_day21.py
+++ python/tests/test2023/test_day21.py
@@ -29,6 +29,6 @@ def test_sample_input(sample_input, steps, gardens):
     assert gardens_within(grid, start, steps) == gardens
 
 
-@mark.parametrize('steps, gardens', [(6, 16), (10, 50), (50, 1594), (100, 6536)])#, (500, 167004)])
+@mark.parametrize('steps, gardens', [(6, 16), (10, 50), (50, 1594), (100, 6536), (500, 167004), (1000, 668697)])
 def test_infinite_gardens(steps, gardens):
     assert infinite_gardens(sample.splitlines(), (5, 5), steps)[-1] == gardens
