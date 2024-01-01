1e6bc11c73f4fe2e2bb09f71865db3edc4f4f7e2
Jared Summers <jtsummers@gmail.com>
2024-01-01 12:43:10 -0700

    [Python] Runner & requirements.txt
    Extracted `load_and_run` to its own function. Set up a test for the `main` method in the loaded modules.
    
    This is step one towards changing my python runner, but I'm not willing to spend all the time now to continue.

--- python/main.py
+++ python/main.py
@@ -6,16 +6,23 @@ def main():
     for year in range(2022, 2024):
         year_start = datetime.datetime.now()
         for day in range(1, 26):
-            try:
-                m = importlib.import_module(f'aoc{year}.day{day:02}')
-                start = datetime.datetime.now()
-                m.main()
-                end = datetime.datetime.now()
-                print(f'[{year}.{day:02}: {end - start}]')
-            except ModuleNotFoundError:
-                pass
+            load_and_run(year, day)
         year_end = datetime.datetime.now()
-        print(f'[{year}: {year_end-year_start}]')
+        print(f'[{year}: {year_end - year_start}]')
+
+
+def load_and_run(year, day):
+    try:
+
+        m = importlib.import_module(f'aoc{year}.day{day:02}')
+        print(m.__file__)
+        start = datetime.datetime.now()
+        if hasattr(m, 'main'):
+            m.main()
+        end = datetime.datetime.now()
+        print(f'[{year}.{day:02}: {end - start}]')
+    except ModuleNotFoundError:
+        pass
 
 
 if __name__ == "__main__":
