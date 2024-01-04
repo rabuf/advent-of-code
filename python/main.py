e3fee2a2789c49bdf1638c54e2ec2653d9d6e2c3
Jared Summers <jtsummers@gmail.com>
2024-01-04 08:49:49 -0700

    [Python] `main.py` updated
    Runs all problems and all years if there are solutions. Starts in 2015 and runs through 2023, days 1-25.
    
    It also times each day and year, if the year has no solutions it displays no timing data.

--- python/main.py
+++ python/main.py
@@ -3,26 +3,28 @@ import importlib
 
 
 def main():
-    for year in range(2022, 2024):
+    for year in range(2015, 2024):
         year_start = datetime.datetime.now()
+        days = 0
         for day in range(1, 26):
-            load_and_run(year, day)
+            days += load_and_run(year, day)
         year_end = datetime.datetime.now()
-        print(f'[{year}: {year_end - year_start}]')
+        if days:
+            print(f'[{year}: {year_end - year_start}]')
 
 
 def load_and_run(year, day):
     try:
 
         m = importlib.import_module(f'aoc{year}.day{day:02}')
-        print(m.__file__)
         start = datetime.datetime.now()
         if hasattr(m, 'main'):
             m.main()
         end = datetime.datetime.now()
         print(f'[{year}.{day:02}: {end - start}]')
+        return True
     except ModuleNotFoundError:
-        pass
+        return False
 
 
 if __name__ == "__main__":
