7e56396cbb90cd6d95ff557c593860822f858200
Jared Summers <jtsummers@gmail.com>
2024-12-01 22:51:59 -0700

    [Pythnon][2024][02] Both parts

--- python/aoc2023/template.py
+++ python/aoc2023/template.py
@@ -10,7 +10,7 @@ def parse_line(line):
 
 def main():
     input_dir = Path(sys.argv[1])
-    with open(input_dir / "2023" / "01.txt") as f:
+    with open(input_dir / "2024" / "01.txt") as f:
         lines = list(map(parse_line, f.read().splitlines()))
         print_day(1, len(lines), len(lines))
 
---
+++ python/aoc2024/day02.py
@@ -0,0 +1,35 @@
+import sys
+from pathlib import Path
+
+from aoc_util import print_day
+
+
+def parse_line(line):
+    return [int(i) for i in line.split()]
+
+
+def safe(record):
+    differences = [a - b for a, b in zip(record, record[1:])]
+    return ((all(n > 0 for n in differences) or all(n < 0 for n in differences))
+            and all(1 <= abs(n) <= 3 for n in differences))
+
+
+def make_safe(record):
+    if safe(record): return True
+    for i in range(len(record)):
+        r = record[0:i] + record[i+1:]
+        if safe(r): return True
+    return False
+
+
+def main():
+    input_dir = Path(sys.argv[1])
+    with open(input_dir / "2024" / "02.txt") as f:
+        lines = list(map(parse_line, f.read().splitlines()))
+        p1 = sum(map(safe, lines))
+        p2 = sum(map(make_safe, lines))
+        print_day(1, p1, p2)
+
+
+if __name__ == '__main__':
+    main()
