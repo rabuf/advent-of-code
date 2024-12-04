375483fad4f552a08ed555c525b2f6509aa1b96e
Jared Summers <jtsummers@gmail.com>
2024-12-03 22:39:19 -0700

    [Python][2024][03] Alternate part 2 (as part 3)

--- python/aoc2024/day03.py
+++ python/aoc2024/day03.py
@@ -33,13 +33,30 @@ def part2(operations):
     return result
 
 
+def part3(operations):
+    result = 0
+    enabled = True
+    pattern = re.compile(r"(mul)\((\d+),(\d+)\)|(do)\(\)|(don't)\(\)")
+    for operation in operations:
+        op = list(filter(None,re.match(pattern, operation).groups()))
+        match op:
+            case ["mul", lhs, rhs]:
+                result += enabled * int(lhs) * int(rhs)
+            case ["do"]:
+                enabled = True
+            case ["don't"]:
+                enabled = False
+    return result
+
+
 def main():
     input_dir = Path(sys.argv[1])
     with open(input_dir / "2024" / "03.txt") as f:
         lines = list(chain(*list(map(parse_line, f.read().splitlines()))))
         p1 = part1(lines)
         p2 = part2(lines)
-        print_day(1, p1, p2)
+        p3 = part3(lines)
+        print_day(1, p1, p2, p3)
 
 
 if __name__ == '__main__':
