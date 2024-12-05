f0f0313d573bf493514cb239033daac561bd2f40
Jared Summers <jtsummers@gmail.com>
2024-12-05 00:13:56 -0700

    [Python][2024][05] Too fancy version

--- python/aoc2024/day05.py
+++ python/aoc2024/day05.py
@@ -10,31 +10,11 @@ from aoc_util import print_day
 
 
 def parse_rules(rules: str):
-    result = defaultdict(list)
+    result = defaultdict(set)
     for rule in rules.splitlines():
         l, r = list(map(int, rule.split('|')))
-        result[l].append(r)
+        result[l].add(r)
     return result
-
-
-def valid_printout(rules: dict[int,list[int]], printout):
-    def key(a, b):
-        if a == b:
-            return 0
-        if a in rules and b in rules[a]:
-            return -1
-        return 1
-    return is_sorted(printout, key=cmp_to_key(key))
-
-
-def fix_printout(rules, printout):
-    def key(a, b):
-        if a == b:
-            return 0
-        if a in rules and b in rules[a]:
-            return -1
-        return 1
-    return sorted(printout, key=cmp_to_key(key))
 
 
 def main():
@@ -42,10 +22,18 @@ def main():
     with open(input_dir / "2024" / "05.txt") as f:
         rules, printouts = f.read().split('\n\n')
         rules = parse_rules(rules)
+
+        def key(a, b):
+            if a == b:
+                return 0
+            if a in rules and b in rules[a]:
+                return -1
+            return 1
+
         printouts = [list(map(int, printout.split(','))) for printout in printouts.splitlines()]
 
-        valid = partial(valid_printout, rules)
-        fix = partial(fix_printout, rules)
+        valid = partial(is_sorted, key=cmp_to_key(key))
+        fix = partial(sorted, key=cmp_to_key(key))
         invalids, valids = partition(valid, printouts)
         p1 = sum(p[len(p) // 2] for p in valids)
         p2 = sum(p[len(p) // 2] for p in map(fix, invalids))
