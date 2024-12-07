2d591236e8863fb16ab9af5b2e85203b7f432763
Jared Summers <jtsummers@gmail.com>
2024-12-07 10:22:12 -0700

    [Python][2024][07] Much faster, all days now under 1 second.

--- python/aoc2024/day07.py
+++ python/aoc2024/day07.py
@@ -1,5 +1,5 @@
 import sys
-from operator import mul, add
+from operator import mul, add, sub, floordiv
 from pathlib import Path
 from math import log10, floor
 
@@ -9,6 +9,34 @@ from aoc_util import print_day
 def parse_line(line):
     result, operands = line.split(':')
     return int(result), list(int(operand) for operand in operands.split())
+
+
+def check_inverse_concatenate(x, current):
+    return current % 10 ** floor(log10(x) + 1) == x
+
+
+def check_sub(x, current):
+    return True
+
+
+def check_div(x, current):
+    return current % x == 0
+
+
+def reverse_search(expected, operands, operations=(sub, floordiv), checks=(check_sub, check_div)):
+    op_checks = list(zip(operations, checks))
+    def recur(current, *rest):
+        if not rest:
+            if current == 0:
+                return True
+            else:
+                return False
+        for op, check in op_checks:
+            if check(rest[0], current):
+                if recur(op(current, rest[0]), *rest[1:]):
+                    return True
+        return False
+    return recur(expected, *operands)
 
 
 def find_operands(expected, operands, operations=(mul, add)):
@@ -28,15 +56,23 @@ def find_operands(expected, operands, operations=(mul, add)):
     return recur(*operands)
 
 
-catenate = lambda a, b: a * 10 ** floor(log10(b) + 1) + b
+def concatenate(a, b):
+    return a * 10 ** floor(log10(b) + 1) + b
+
+
+def inverse_concatenate(a, b):
+    return a // 10 ** floor(log10(b) + 1)
 
 
 def main():
     input_dir = Path(sys.argv[1])
     with open(input_dir / "2024" / "07.txt") as f:
         lines = list(map(parse_line, f.read().splitlines()))
-        p1 = sum(expected for (expected, operands) in lines if find_operands(expected, operands))
-        p2 = sum(expected for (expected, operands) in lines if find_operands(expected, operands, (mul, add, catenate)))
+        p1 = sum(expected for (expected, operands) in lines if reverse_search(expected, reversed(operands)))
+        p2 = sum(expected for (expected, operands) in lines
+                 if reverse_search(expected, reversed(operands),
+                                   (sub, floordiv, inverse_concatenate),
+                                   (check_sub, check_div, check_inverse_concatenate)))
         print_day(7, p1, p2)
 
 
