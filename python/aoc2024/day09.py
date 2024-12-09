d63d9ef501657fec5a6b373fbb3a31239ee68480
Jared Summers <jtsummers@gmail.com>
2024-12-09 00:13:24 -0700

    [Python][2024][09] Both parts

---
+++ python/aoc2024/day09.py
@@ -0,0 +1,86 @@
+import sys
+from collections import defaultdict
+from itertools import takewhile
+from pathlib import Path
+
+from aoc_util import print_day
+
+
+def parse_line(line):
+    result = []
+    position = 0
+    for i, length in enumerate(line):
+        l = int(length)
+        if i % 2 == 0:
+            result[position:position + l] = [i//2] * l
+        else:
+            result[position:position + l] = [None] * l
+        position += l
+    return result
+
+
+def compact(files: list):
+    compacted = files[:]
+    l, r = 0, len(files) - 1
+    while l < r:
+        while compacted[r] is None:
+            r = r - 1
+        while compacted[l] is not None:
+            l = l + 1
+        if l < r:
+            compacted[l], compacted[r] = compacted[r], compacted[l]
+    return compacted
+
+
+def checksum(files: list):
+    return sum(i * file for i, file in enumerate(files) if file is not None)
+
+
+def compact_whole_files(files, file_blocks, free_blocks: dict):
+    compacted = files[:]
+    for file_number in sorted(file_blocks, reverse=True):
+        start, length = file_blocks[file_number]
+        for p in sorted(free_blocks):
+            if p > start:
+                break
+            if free_blocks[p] >= length:
+                compacted[p:p + length] = [file_number] * length
+                compacted[start:start + length] = [None] * length
+                l = free_blocks[p]
+                if length < l:
+                    free_blocks[p + length] = l - length
+                del free_blocks[p]
+                break
+    return compacted
+
+
+def to_file_blocks(line):
+    blocks = {}
+    free = {}
+    position = 0
+    for i, length in enumerate(line):
+        l = int(length)
+        if i % 2 == 0:
+            blocks[i//2] = (position, l)
+            position = position + l
+        else:
+            free[position] = l
+            position = position + l
+    return blocks, free
+
+
+def main():
+    input_dir = Path(sys.argv[1])
+    with open(input_dir / "2024" / "09.txt") as f:
+        line = f.readline().strip()
+    files = parse_line(line)
+    p1_compacted = compact(files)
+    p1 = checksum(p1_compacted)
+    blocks, free = to_file_blocks(line)
+    p2_compacted = compact_whole_files(files, blocks, free)
+    p2 = checksum(p2_compacted)
+    print_day(9, p1, p2)
+    assert p2 == 6377400869326
+
+if __name__ == '__main__':
+    main()
---
+++ python/tests/test2024/test_day09.py
@@ -0,0 +1,17 @@
+import pytest
+
+from aoc2024.day09 import *
+
+@pytest.fixture()
+def sample():
+    return parse_line('2333133121414131402')
+
+def test_part1(sample):
+    compacted = compact(sample)
+    assert 1928 == checksum(compacted)
+
+
+def test_part2(sample):
+    files, free = to_file_blocks('2333133121414131402')
+    sample_compacted = compact_whole_files(sample, files, free)
+    assert 2858 == checksum(sample_compacted)
