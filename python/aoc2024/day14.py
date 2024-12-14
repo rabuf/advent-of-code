874eaeb800127721ce73ffacbd200ce560ce50a1
Jared Summers <jtsummers@gmail.com>
2024-12-13 22:46:46 -0700

    [Python][2024][14] Both parts
    I'll say I got lucky on part 2. I was working through patterns and printing out "interesting" arrangements and stumbled on this heuristic.

--- python/aoc2024/day14.py
+++ python/aoc2024/day14.py
@@ -1,11 +1,45 @@
+import re
 import sys
+from functools import partial
 from pathlib import Path
 
 from aoc_util import print_day
 
 
+def future(px, py, vx, vy, time=100):
+    x = (px + vx * time) % 101
+    y = (py + vy * time) % 103
+    return x, y
+
+
+def safety_factor(positions):
+    q1, q2, q3, q4 = 0, 0, 0, 0
+    for x, y in positions:
+        qx, qy = x - 50, y - 51
+        if qx < 0 < qy:
+            q1 = q1 + 1
+        if qx > 0 and qy > 0:
+            q2 = q2 + 1
+        if qx > 0 > qy:
+            q3 = q3 + 1
+        if qx < 0 and qy < 0:
+            q4 = q4 + 1
+    print(q1, q2, q3, q4)
+    return q1 * q2 * q3 * q4
+
 def parse_line(line):
-    return line
+    return tuple(map(int,re.findall(r'[+-]?\d+', line)))
+
+
+def print_positions(positions: list):
+    for y in range(103):
+        for x in range(101):
+            p = x, y
+            if p in positions:
+                print(positions.count(p), end='')
+            else:
+                print('.', end='')
+        print()
 
 
 def main():
@@ -13,7 +47,16 @@ def main():
     try:
         with open(input_dir / "2024" / "14.txt") as f:
             lines = list(map(parse_line, f.read().splitlines()))
-            print_day(14, len(lines), len(lines))
+            positions = list(map(future, *zip(*lines)))
+            p1 = safety_factor(positions)
+            time = 0
+            while True:
+                positions = list(map(partial(future, time=time), *zip(*lines)))
+                if len(set(positions)) == len(positions):
+                    print_positions(positions)
+                    break
+                time = time + 1
+            print_day(14, p1, time)
     except IOError as e:
         print(e)
 
