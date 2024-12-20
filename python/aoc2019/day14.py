8f6340af33d015d0529ea58e145f17b1b072c59b
Jared Summers <jtsummers@gmail.com>
2024-12-19 21:51:49 -0700

    [Python][2019][14] Part 1

---
+++ python/aoc2019/day14.py
@@ -0,0 +1,57 @@
+import sys
+from collections import defaultdict
+from pathlib import Path
+
+from aoc_util import print_day
+
+
+def parse_line(line):
+    inputs, output = line.split('=>')
+    inputs = inputs.split(',')
+    inputs = [i.strip().split(' ') for i in inputs]
+    inputs = [(int(l), r) for l, r in inputs]
+    l, r = output.strip().split(' ')
+    output = int(l), r
+    return inputs, output
+
+
+def required_ore(rules, fuel=1):
+    available = defaultdict(int)
+    consumed = defaultdict(int)
+
+    def recur(n, item):
+        if item == 'ORE':
+            consumed['ORE'] += n
+        elif available[item] >= n:
+            available[item] -= n
+            consumed[item] += n
+        else:
+            c, items = rules[item]
+            for m, i in items:
+                recur(m, i)
+            available[item] += c
+            recur(n, item)
+
+    recur(fuel, 'FUEL')
+    return consumed['ORE'] + available['ORE']
+
+
+def main():
+    input_dir = Path(sys.argv[1])
+    try:
+        with open(input_dir / "2019" / "14.txt") as f:
+            lines = list(map(parse_line, f.read().splitlines()))
+        rules = {}
+        for i, (n, o) in lines:
+            rules[o] = (n, i)
+        p1 = required_ore(rules)
+        p2 = p1
+        fuel = 1
+
+        print_day("14", p1, p2, lines, len(lines))
+    except IOError as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
