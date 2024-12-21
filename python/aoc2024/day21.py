93387b28f21d792dda53287f87fce9ddfcd67f17
Jared Summers <jtsummers@gmail.com>
2024-12-21 10:55:53 -0700

    [Python][2024][21] Part 1 - closer to the target, even faster
    Time down below 1ms. This one only generates the desired paths but generates too much redundant stuff for part 2.

--- python/aoc2024/day21.py
+++ python/aoc2024/day21.py
@@ -1,6 +1,7 @@
 import math
 import sys
 from itertools import zip_longest
+from functools import cache
 from pathlib import Path
 
 import networkx
@@ -58,19 +59,36 @@ dir_to_arrow = {
 arrow_to_dir = {v: k for k, v in dir_to_arrow.items()}
 
 
-def recursive_direction_generator(G, buttons, to_pos, to_key, start='A'):
-    def recur(pos, bs):
-        if bs:
-            dest = bs[0]
-            options = networkx.all_shortest_paths(G, to_pos[pos], to_pos[dest])
-            for o in options:
-                for rest in recur(dest, bs[1:]):
-                    yield o + rest
-        else:
-            yield []
+@cache
+def optimal_single(start, end, depth):
+    if depth == 0:
+        return optimal_directions(pos_to_key[start], key_to_pos, pos_to_key[end])
 
-    for path in recur(start, buttons):
-        yield ''.join(dir_to_arrow[b - a] for a, b in zip(path, path[1:])) + 'A'
+
+def optimal_directions(buttons, to_pos, start='A'):
+    path = []
+    for a, b in zip(start + buttons, buttons):
+        distance = to_pos[b] - to_pos[a]
+        x, y = int(distance.real), int(distance.imag)
+        xs = (x > 0) - (x < 0)
+        ys = ((0 < y) - (y < 0)) * 1j
+        match x, y:
+            case 0, 0:
+                pass
+            case x, 0:
+                path.append(dir_to_arrow[xs] * abs(x))
+            case 0, y:
+                path.append(dir_to_arrow[ys] * abs(y))
+            case x, y if to_pos[a] + x == -2:
+                path.append(dir_to_arrow[ys] * abs(y) + dir_to_arrow[xs] * abs(x))
+            case x, y if to_pos[a] + y * 1j == -2:
+                path.append(dir_to_arrow[xs] * abs(x) + dir_to_arrow[ys] * abs(y))
+            case x, y if x < 0:
+                path.append(dir_to_arrow[xs] * abs(x) + dir_to_arrow[ys] * abs(y))
+            case x, y:
+                path.append(dir_to_arrow[ys] * abs(y) + dir_to_arrow[xs] * abs(x))
+        path.append('A')
+    return ''.join(path)
 
 
 def directions(G, buttons, to_pos, pos='A'):
@@ -85,10 +103,11 @@ def directions(G, buttons, to_pos, pos='A'):
     return ''.join(result)
 
 
-def apply_arrows(G, directions, to_name):
+def apply_arrows(directions, to_name):
     pos = 0
     result = []
     for d in directions:
+        if pos not in to_name: raise ValueError(pos)
         if d == 'A':
             result.append(to_name[pos])
         else:
@@ -104,12 +123,21 @@ def main():
 
         p1 = 0
         for digits in lines:
-            part_min = part1(digits)
+            r = optimal_directions(digits, key_to_pos)
+            for _ in range(2):
+                r = optimal_directions(r, arrow_to_pos)
             n = int(digits[:-1])
-            p1 += part_min * n
-            print(p1)
-
-        print_day("21", p1, 2, len(lines))
+            p1 += n * len(r)
+        p2 = 0
+        # This grows way too big, what's a better way?
+        # for digits in lines:
+        #     r = optimal_directions(digits[0], key_to_pos)
+        #     for _ in range(13):
+        #         r = optimal_directions(r, arrow_to_pos)
+        #     n = int(digits[:-1])
+        #     p2 += n * len(r)
+        print_day("21", p1, p2)
+        assert p1 == 163086
     except IOError as e:
         print(e)
 
@@ -118,9 +146,9 @@ def part1(digits):
     part_min = 0
     for a, b in zip('A' + digits, digits):
         r3_min = math.inf
-        for r1 in recursive_direction_generator(key_graph, b, key_to_pos, pos_to_arrow, a):
-            for r2 in recursive_direction_generator(arrow_graph, r1, arrow_to_pos, pos_to_arrow):
-                for r3 in recursive_direction_generator(arrow_graph, r2, arrow_to_pos, pos_to_arrow):
+        for r1 in optimal_directions(key_graph, b, key_to_pos, pos_to_arrow, a):
+            for r2 in optimal_directions(arrow_graph, r1, arrow_to_pos, pos_to_arrow):
+                for r3 in optimal_directions(arrow_graph, r2, arrow_to_pos, pos_to_arrow):
                     r3_min = min(len(r3), r3_min)
                     if len(r3) >= r3_min: break
         part_min += r3_min
