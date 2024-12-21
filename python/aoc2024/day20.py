f8f309436c681f7d205c909d558497c29bc1c7ec
Jared Summers <jtsummers@gmail.com>
2024-12-20 19:11:37 -0700

    [Python][2024][20] Part 2
    I still think I can make this faster, it runs in ~1.6 seconds on my laptop. But I'm not sure I want to spend much more on it.

--- python/aoc2024/day20.py
+++ python/aoc2024/day20.py
@@ -1,5 +1,4 @@
 import sys
-from itertools import combinations
 from pathlib import Path
 
 from aoc_util import print_day
@@ -10,15 +9,30 @@ def parse_line(line):
     return line
 
 
-def part2(G, walls, start, end):
-    cheats = set()
-    W = networkx.Graph()
-    W.add_nodes_from(walls)
-    for node in W.nodes:
-        for n in (node + d for d in (1, -1, 1j, -1j)):
-            if n in W.nodes:
-                W.add_edge(node, n)
-    return len(cheats)
+def manhattan(a, b):
+    return int(abs(a.imag - b.imag) + abs(a.real - b.real))
+
+
+def offsets(n):
+    result = set()
+    for i in range(n + 1):
+        for j in range(n + 1 - i):
+            result.add(complex(i, j))
+            result.add(complex(i, -j))
+            result.add(complex(-i, j))
+            result.add(complex(-i, -j))
+    return result - {0}
+
+
+def cheats(G, start, limit=20, threshold=50):
+    cheat_count = 0
+    distances = networkx.single_source_shortest_path_length(G, start)
+    for a in G:
+        for b in (b for o in offsets(limit) if (b := a + o) in G and distances[b] - distances[a] >= threshold):
+            m = abs(a.imag - b.imag) + abs(a.real - b.real)
+            if distances[b] - distances[a] >= threshold + m:
+                cheat_count = cheat_count + 1
+    return cheat_count
 
 
 def main():
@@ -27,38 +41,24 @@ def main():
         with open(input_dir / "2024" / "20.txt") as f:
             lines = list(map(parse_line, f.read().splitlines()))
         G = networkx.Graph()
-        walls = []
-        start = end = 0
+        start = 0
         for j, line in enumerate(lines):
             for i, c in enumerate(line):
-                pos = i + j * 1j
+                pos = complex(i, j)
                 match c:
                     case 'S':
                         start = pos
                         G.add_node(pos)
-                    case 'E':
-                        end = pos
-                        G.add_node(pos)
-                    case '.':
+                    case '.' | 'E':
                         G.add_node(pos)
                     case _:
-                        walls.append(pos)
+                        pass
         for node in G.nodes:
             for n in (node + d for d in (1, -1, 1j, -1j) if node + d in G):
                 G.add_edge(node, n)
-        from_start = networkx.single_source_shortest_path_length(G, start)
-        from_end = networkx.single_source_shortest_path_length(G, end)
-        limit = from_start[end]
-        p1 = 0
-        for w in walls:
-            neighbors = [w + d for d in (1, -1, 1j, -1j) if w + d in G]
-            if len(neighbors) >= 2:
-                for a, b in zip(neighbors, neighbors[1:]):
-                    distance = min(from_start[a] + from_end[b], from_start[b] + from_end[a]) + 1
-                    if limit - distance >= 100:
-                        p1 += 1
-
-        print_day("20", p1, G, len(lines))
+        p1 = cheats(G, start, limit=2, threshold=100)
+        p2 = cheats(G, start, limit=20, threshold=100)
+        print_day("20", p1, p2)
     except IOError as e:
         print(e)
 
