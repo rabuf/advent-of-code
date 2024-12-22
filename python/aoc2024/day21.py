ec65f39da30a8320d3c6978cf77a0169fa42459e
Jared Summers <jtsummers@gmail.com>
2024-12-21 18:15:47 -0700

    [Python][2024][21] Part 2
    Had a damned typo, turned up in my real data but somehow was ok for part 1.

--- python/aoc2024/day21.py
+++ python/aoc2024/day21.py
@@ -1,24 +1,8 @@
-import math
 import sys
-from itertools import zip_longest
 from functools import cache
 from pathlib import Path
 
-import networkx
-
 from aoc_util import print_day
-
-
-
-def make_graph(keys):
-    G = networkx.Graph()
-    G.add_nodes_from(v for _, v in keys.items())
-    for n in G:
-        for d in (n + d for d in (1, -1, 1j, -1j)):
-            if d in G:
-                G.add_edge(n, d)
-    return G
-
 
 key_to_pos = {
     'A': 0,
@@ -42,12 +26,6 @@ arrow_to_pos = {
     '>': -1j,
 }
 
-pos_to_key = {v: k for k, v in key_to_pos.items()}
-pos_to_arrow = {v: k for k, v in arrow_to_pos.items()}
-
-key_graph = make_graph(key_to_pos)
-arrow_graph = make_graph(arrow_to_pos)
-
 dir_to_arrow = {
     0: 'A',
     -1: '<',
@@ -56,63 +34,52 @@ dir_to_arrow = {
     1j: '^',
 }
 
-arrow_to_dir = {v: k for k, v in dir_to_arrow.items()}
-
 
 @cache
-def optimal_single(start, end, depth):
-    if depth == 0:
-        return optimal_directions(pos_to_key[start], key_to_pos, pos_to_key[end])
-
-
-def optimal_directions(buttons, to_pos, start='A'):
+def optimal_direction(start, end):
+    distance = end - start
     path = []
-    for a, b in zip(start + buttons, buttons):
-        distance = to_pos[b] - to_pos[a]
-        x, y = int(distance.real), int(distance.imag)
-        xs = (x > 0) - (x < 0)
-        ys = ((0 < y) - (y < 0)) * 1j
-        match x, y:
-            case 0, 0:
-                pass
-            case x, 0:
-                path.append(dir_to_arrow[xs] * abs(x))
-            case 0, y:
-                path.append(dir_to_arrow[ys] * abs(y))
-            case x, y if to_pos[a] + x == -2:
-                path.append(dir_to_arrow[ys] * abs(y) + dir_to_arrow[xs] * abs(x))
-            case x, y if to_pos[a] + y * 1j == -2:
-                path.append(dir_to_arrow[xs] * abs(x) + dir_to_arrow[ys] * abs(y))
-            case x, y if x < 0:
-                path.append(dir_to_arrow[xs] * abs(x) + dir_to_arrow[ys] * abs(y))
-            case x, y:
-                path.append(dir_to_arrow[ys] * abs(y) + dir_to_arrow[xs] * abs(x))
-        path.append('A')
+    x, y = int(distance.real), int(distance.imag)
+    xs = (x > 0) - (x < 0)
+    ys = ((0 < y) - (y < 0)) * 1j
+    match x, y:
+        case 0, 0:
+            pass
+        case x, 0:
+            path.append(dir_to_arrow[xs] * abs(x))
+        case 0, y:
+            path.append(dir_to_arrow[ys] * abs(y))
+        case x, y if start + x == -2:
+            path.append(dir_to_arrow[ys] * abs(y) + dir_to_arrow[xs] * abs(x))
+        case x, y if start + y * 1j == -2:
+            path.append(dir_to_arrow[xs] * abs(x) + dir_to_arrow[ys] * abs(y))
+        case x, y if x < 0:
+            path.append(dir_to_arrow[xs] * abs(x) + dir_to_arrow[ys] * abs(y))
+        case x, y:
+            path.append(dir_to_arrow[ys] * abs(y) + dir_to_arrow[xs] * abs(x))
+    path.append('A')
     return ''.join(path)
 
 
-def directions(G, buttons, to_pos, pos='A'):
-    result = []
-    for b in buttons:
-        steps = networkx.shortest_path(G, to_pos[pos], to_pos[b])
-        for m, n in zip(steps, steps[1:]):
-            d = n - m
-            result.append(dir_to_arrow[d])
-        result.append('A')
-        pos = b
-    return ''.join(result)
+def solve(digits, depth):
+    @cache
+    def recur(a, b, d):
+        steps = optimal_direction(arrow_to_pos[a], arrow_to_pos[b])
+        if d == 0:
+            return len(steps)
+        result = 0
+        for m, n in zip('A' + steps, steps):
+            result += recur(m, n, d - 1)
+        return result
 
-
-def apply_arrows(directions, to_name):
-    pos = 0
-    result = []
-    for d in directions:
-        if pos not in to_name: raise ValueError(pos)
-        if d == 'A':
-            result.append(to_name[pos])
-        else:
-            pos += arrow_to_dir[d]
-    return ''.join(result)
+    initial = []
+    for a, b in zip('A' + digits, digits):
+        initial.extend(optimal_direction(key_to_pos[a], key_to_pos[b]))
+    initial = ''.join(initial)
+    result = 0
+    for a, b in zip('A' + initial, initial):
+        result += recur(a, b, depth)
+    return result
 
 
 def main():
@@ -120,39 +87,20 @@ def main():
     try:
         with open(input_dir / "2024" / "21.txt") as f:
             lines = f.read().splitlines()
-
-        p1 = 0
-        for digits in lines:
-            r = optimal_directions(digits, key_to_pos)
-            for _ in range(2):
-                r = optimal_directions(r, arrow_to_pos)
-            n = int(digits[:-1])
-            p1 += n * len(r)
-        p2 = 0
-        # This grows way too big, what's a better way?
-        # for digits in lines:
-        #     r = optimal_directions(digits[0], key_to_pos)
-        #     for _ in range(13):
-        #         r = optimal_directions(r, arrow_to_pos)
-        #     n = int(digits[:-1])
-        #     p2 += n * len(r)
+        p1 = fast_solver(1, lines)
+        p2 = fast_solver(24, lines)
         print_day("21", p1, p2)
-        assert p1 == 163086
     except IOError as e:
         print(e)
 
 
-def part1(digits):
-    part_min = 0
-    for a, b in zip('A' + digits, digits):
-        r3_min = math.inf
-        for r1 in optimal_directions(key_graph, b, key_to_pos, pos_to_arrow, a):
-            for r2 in optimal_directions(arrow_graph, r1, arrow_to_pos, pos_to_arrow):
-                for r3 in optimal_directions(arrow_graph, r2, arrow_to_pos, pos_to_arrow):
-                    r3_min = min(len(r3), r3_min)
-                    if len(r3) >= r3_min: break
-        part_min += r3_min
-    return part_min
+def fast_solver(depth, lines):
+    p2 = 0
+    for digits in lines:
+        l = solve(digits, depth)
+        n = int(digits[:-1])
+        p2 += l * n
+    return p2
 
 
 if __name__ == '__main__':
