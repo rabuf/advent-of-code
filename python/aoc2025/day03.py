32f712b281703478bd2ac0da84eda7d941504b32
Jared Summers <jtsummers@gmail.com>
2025-12-02 22:24:59 -0700

    [Python][2025][03] Clean up
    - Switched to `functools.partial` instead of a new function definition.
    
    - Skipped the initial splitting and removed joining logic. It's simpler to work with the line as a string, not a list of
    digits.

--- python/aoc2025/day03.py
+++ python/aoc2025/day03.py
@@ -1,22 +1,19 @@
 import sys
+from functools import partial
 from pathlib import Path
 
 from aoc_util import print_day
 
 
-def parse_line(line):
-    return [int(i) for i in line]
-
-
 def best_joltage(bank, size=2):
     joltage, rest = bank[:size], bank[size:]
     for c in rest:
-        best = int("".join(map(str, joltage)))
+        best = int(joltage)
         for i in range(size):
-            contender = int("".join(map(str, joltage[:i] + joltage[i + 1 :] + [c])))
+            contender = int(joltage[:i] + joltage[i + 1 :] + f"{c}")
             best = max(best, contender)
-        joltage = list(map(int, str(best)))
-    return int("".join(map(str, joltage)))
+        joltage = str(best)
+    return int(joltage)
 
 
 def part1(banks):
@@ -24,18 +21,15 @@ def part1(banks):
 
 
 def part2(banks):
-    def best(b):
-        return best_joltage(b, 12)
-
-    return sum(map(best, banks))
+    return sum(map(partial(best_joltage, size=12), banks))
 
 
 def main():
     input_dir = Path(sys.argv[1])
     try:
         with open(input_dir / "2025" / "03.txt") as f:
-            lines = list(map(parse_line, f.read().splitlines()))
-        print_day("03", part1(lines), part2(lines))
+            banks = f.read().splitlines()
+        print_day("03", part1(banks), part2(banks))
     except IOError as e:
         print(e)
 
