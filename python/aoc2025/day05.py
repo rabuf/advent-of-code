7927e84764b1b29196d6d9a354392b68fa3237fc
Jared Summers <jtsummers@gmail.com>
2025-12-04 23:26:40 -0700

    [Python][2025][05] Fixed some goofs.

--- python/aoc2025/day05.py
+++ python/aoc2025/day05.py
@@ -14,17 +14,13 @@ def cmp_ranges(a, b):
         return -1
     if a[0] > b[0]:
         return 1
-    if a[-1] < b[-1]:
-        return -1
-    if a[-1] > b[-1]:
-        return -1
     return 0
 
 
 def part2(fresh):
     fresh = list(sorted(fresh, key=cmp_to_key(cmp_ranges)))
-    merged = [range(fresh[0][0], fresh[0][-1] + 1)]
-    for start, end in [(r[0], r[-1]) for r in fresh]:
+    merged = fresh[:1]
+    for start, end in [(r[0], r[-1]) for r in fresh[1:]]:
         merge_start, merge_end = merged[-1][0], merged[-1][-1]
         if start <= merge_end:
             merged[-1] = range(merge_start, max(merge_end, end) + 1)
@@ -42,7 +38,7 @@ def main(input_dir=Path(sys.argv[1])):
             for lower, upper in (f.split("-") for f in fresh.splitlines())
         ]
         ingredients = map(int, ingredients.splitlines())
-        print_day("DAY", part1(fresh, ingredients), part2(fresh))
+        print_day("05", part1(fresh, ingredients), part2(fresh))
     except IOError as e:
         print(e)
 
