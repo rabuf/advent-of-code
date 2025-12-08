b04960f65d63ef96fb7ab84477f5b50f5feb048b
Jared Summers <jtsummers@gmail.com>
2025-12-07 22:35:29 -0700

    [Python][2025][08] Both parts.
    I need to improve part 2, it's  slow but does the job.

---
+++ python/aoc2025/day08.py
@@ -0,0 +1,70 @@
+import heapq
+import sys
+from functools import reduce
+from operator import mul
+from pathlib import Path
+
+import networkx as nx
+
+from aoc_util import print_day
+
+YEAR = "2025"
+DAY = "08"
+
+
+def parse_line(line):
+    return tuple(map(int, line.split(",")))
+
+
+def all_pairs_distances(junction_boxes):
+    ordered = []
+    for i, (ax, ay, az) in enumerate(junction_boxes):
+        for bx, by, bz in junction_boxes[i + 1 :]:
+            a = (ax, ay, az)
+            b = (bx, by, bz)
+            distance = ((ax - bx) ** 2 + (ay - by) ** 2 + (az - bz) ** 2) ** (1 / 2)
+            heapq.heappush(ordered, (distance, a, b))
+    return ordered
+
+
+def part1(boxes):
+    ordered = all_pairs_distances(boxes)
+    graph = nx.Graph()
+
+    for box in boxes:
+        graph.add_node(box)
+
+    for i in range(1000):
+        _, a, b = heapq.heappop(ordered)
+        graph.add_edge(a, b)
+    top3 = sorted((len(s) for s in nx.connected_components(graph)), reverse=True)[:3]
+
+    return reduce(mul, top3)
+
+
+def part2(boxes):
+    ordered = all_pairs_distances(boxes)
+    graph = nx.Graph()
+
+    for box in boxes:
+        graph.add_node(box)
+
+    while nx.number_connected_components(graph) > 1:
+        _, a, b = heapq.heappop(ordered)
+        graph.add_edge(a, b)
+        if nx.number_connected_components(graph) == 1:
+            return a[0] * b[0]
+    return 0
+
+
+def main(input_dir=Path(sys.argv[1])):
+    try:
+        with open(input_dir / YEAR / f"{DAY}.txt") as f:
+            lines = list(map(parse_line, f.read().splitlines()))
+        print_day(DAY, part1(lines), part2(lines))
+    except IOError as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
